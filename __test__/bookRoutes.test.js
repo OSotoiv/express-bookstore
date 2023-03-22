e285fb6ecba0a84bc18a08effe8dfa3c4f27d89e
OctavianoSoto <osoto.iv@gmail.com>
2023-03-22 00:22:15 -0500

    done

--- __test__/bookRoutes.test.js
+++ __test__/bookRoutes.test.js
@@ -14,7 +14,7 @@ describe("class Book Model Testing", () => {
         ('3456789012', 'https://www.amazon.com/Book3', 'Bob Johnson', 'Spanish', 150, 'Publisher3', 'Book Three', 2020),
         ('4567890123', 'https://www.amazon.com/Book4', 'Sarah Lee', 'German', 200, 'Publisher4', 'Book Four', 2019),
         ('5678901234', 'https://www.amazon.com/Book5', 'David Kim', 'Chinese', 350, 'Publisher5', 'Book Five', 2018);`)
-    })
+    });
     test("GET /books/", async () => {
         const response = await request(app)
             .get(`/books/`)
@@ -26,7 +26,7 @@ describe("class Book Model Testing", () => {
                 title: expect.any(String)
             })
         )
-    })
+    });
     test("GET /books/:id", async () => {
         const response = await request(app)
             .get(`/books/1234567890`)
@@ -38,14 +38,14 @@ describe("class Book Model Testing", () => {
                 title: expect.any(String)
             })
         )
-    })
+    });
     test("GET /books/:id With invalid id", async () => {
         const badID = "1234569999";
         const response = await request(app)
             .get(`/books/${badID}`)
         expect(response.statusCode).toBe(404);
         expect(response.body.message).toBe(`There is no book with an isbn ${badID}`)
-    })
+    });
     test("POST /books/", async () => {
         const response = await request(app)
             .post(`/books/`)
@@ -67,7 +67,7 @@ describe("class Book Model Testing", () => {
                 title: expect.any(String)
             })
         )
-    })
+    });
     test("POST /books/ With invalid body", async () => {
         const response = await request(app)
             .post(`/books/`)
@@ -84,7 +84,7 @@ describe("class Book Model Testing", () => {
         expect(response.statusCode).toBe(400)
         expect(response.body.message).toContainEqual('instance.author does not meet minimum length of 3')
         expect(response.body.message).toContainEqual('instance.title does not meet minimum length of 3')
-    })
+    });
     test("PUT /books/:isbn", async () => {
         const response = await request(app)
             .put(`/books/posttest123`)
@@ -105,7 +105,7 @@ describe("class Book Model Testing", () => {
             })
         )
 
-    })
+    });
     test("PUT /books/:isbn With invalid isbn", async () => {
         const badISBN = '5678900000'
         const response = await request(app)
@@ -120,7 +120,7 @@ describe("class Book Model Testing", () => {
                 "year": 2022
             })
         expect(response.body.message).toBe(`There is no book with an isbn ${badISBN}`)
-    })
+    });
     test("PUT /books/:isbn With missing properties. Should default to original properties", async () => {
         const goodISBN = '5678901234';
         const response = await request(app)
@@ -143,7 +143,7 @@ describe("class Book Model Testing", () => {
                 publisher: "UPDated Publishing5"
             })
         )
-    })
+    });
     test("DELETE /books/:isbn", async () => {
         const isbn = 'posttest123'
         const response = await request(app)
@@ -152,7 +152,7 @@ describe("class Book Model Testing", () => {
         expect(response.body.message).toBe('Book deleted')
         const book = await db.query(`SELECT * FROM books WHERE isbn = $1`, [isbn])
         expect(book.rows).toHaveLength(0);
-    })
+    });
     test("DELETE /books/:isbn With invalid ISBN", async () => {
         const badISBN = '5678900000'
         const response = await request(app)
@@ -162,7 +162,7 @@ describe("class Book Model Testing", () => {
 
         const book = await db.query(`SELECT * FROM books WHERE isbn = $1`, [badISBN])
         expect(book.rows).toHaveLength(0);
-    })
+    });
 
 })
 afterAll(async function () {
